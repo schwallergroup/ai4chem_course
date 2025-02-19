3c0c05f53c88578f32e88cf63f345182841dab4d
victorsabanzagil <victorsabanza@gmail.com>
2025-02-19 14:51:55 +0100

    update fingerprint generation
    there was a deprecation warning with the previous approach

--- notebooks/solutions/solution_01d_03.py
+++ notebooks/solutions/solution_01d_03.py
@@ -1,8 +1,8 @@
 toluene = Chem.MolFromSmiles('c1ccccc1C') #insert toluene SMILES
 
 #Now, create the fingerprints of theobromine and toluene
-toluene_fp = AllChem.GetMorganFingerprintAsBitVect(toluene, 2, nBits=1024) #insert corresponding values
-theobromine_fp =  AllChem.GetMorganFingerprintAsBitVect(theobromine, 2, nBits=1024) #same for theobromine
+toluene_fp = mfpgen.GetFingerprint(toluene) #insert corresponding values
+theobromine_fp =  mfpgen.GetFingerprint(theobromine) #same for theobromine
 
 #Now we calculate Tanimoto Similarity
 sim1 = FingerprintSimilarity(caffeine_fp, toluene_fp) #insert fingerprints to compare
